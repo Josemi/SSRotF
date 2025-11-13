12b15c6f1cefee9ffd48f9fb063858550743ee6b
José Miguel Ramírez Sanz <jmrsanz@ubu.es>
2025-11-13 17:12:55 +0100

    Fix script to use correct requirements and example file

--- run_example.sh
+++ run_example.sh
@@ -3,5 +3,5 @@ git clone https://github.com/Josemi/SSRotF.git
 conda create -n SSRotF python=3.10.13
 conda activate SSRotF
 cd SSRotF
-pip install -r requirements
-python run_example.py
+pip install -r requirements.txt
+python example.py
